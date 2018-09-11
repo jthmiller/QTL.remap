ede4b8dc6a9ca347fa22365447aa4c2dbd8ccfe9
Jeffrey Miller <jthmiller@ucdavis.edu>
2018-09-11 02:49:31 -0400

    add qtlwait afterok ifis

--- 18.8.22.map_scan.sh
+++ 18.8.22.map_scan.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # first job - no dependencies
-jid1=$(sbatch  --mem=12g 18.8.22.remap_qtl.sh)
+jid1=$(sbatch 18.8.22.remap_qtl.sh)
 
 # multiple jobs can depend on a single job
 jid2=$(sbatch --dependency=afterok:$jid1 18.8.22.scan_plot_rQTL2.sh)
