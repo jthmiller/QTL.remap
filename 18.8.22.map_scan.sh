83e8d157595e8fe54b392051b04fc82405aef312
Jeffrey Miller <jthmiller@ucdavis.edu>
2018-09-12 12:31:33 -0400

    pars again3

--- 18.8.22.map_scan.sh
+++ 18.8.22.map_scan.sh
@@ -8,6 +8,6 @@ scriptdir='/home/jmiller1/QTL_Map_Raw/popgen/rQTL/scripts/QTL_remap'
 
 RES=$(sbatch --parsable $scriptdir/18.8.22.remap_qtl_1.sh)
 
-RES2=$(sbatch --parsable --dependency=afterok:$RES RES2=$(sbatch $scriptdir/18.8.22.remap_qtl_2.sh) 
+RES2=$(sbatch --parsable --dependency=afterok:$RES $scriptdir/18.8.22.remap_qtl_2.sh) 
 
 sbatch --dependency=afterok:$RES2 $scriptdir/18.8.22.remap_qtl_3.sh
