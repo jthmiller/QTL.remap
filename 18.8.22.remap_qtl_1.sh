b5c8ba56f0dcc72649aee9f32e886e55c329151f
Jeffrey Miller <jthmiller@ucdavis.edu>
2018-09-12 02:05:32 -0400

    overhaul3

--- 18.8.22.remap_qtl_1.sh
+++ 18.8.22.remap_qtl_1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -l
-#SBATCH -J array_job
+#SBATCH -J initial_map
 #SBATCH -o /home/jmiller1/QTL_Map_Raw/popgen/rQTL/scripts/array_error_out/array_job_out_%A_%a.txt
 #SBATCH -e /home/jmiller1/QTL_Map_Raw/popgen/rQTL/scripts/array_error_out/array_job_err_%A_%a.txt
 #SBATCH --time=24:00:00
