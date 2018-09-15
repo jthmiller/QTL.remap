731443d5fddb9af507cd1788ee45736fe4b04593
Jeffrey Miller <jthmiller@ucdavis.edu>
2018-09-15 14:48:10 -0400

    fixed par markers

--- MAP/source_file.R
+++ MAP/source_file.R
@@ -73,16 +73,18 @@ keepQTL <- function(Z,i){
   markerVec <- row.names(gt.1[order(abs(pos.m-pos)) < 10,])
   return(markerVec)
 }
-dropone.par <- function(cross,p,chr,map.function = 'kosambi',length.imp = 1, LOD.imp = 0,
-  maxit=1,sex.sp = F,verbose=F,parallel=T,error.prob = 0.03,cores){
-  y <- round(sum(nmar(cross))*p)
-  ### p = percent of longest markers to drop
-  cross.drops <- parallel.droponemarker(cross,
-        chr, map.function = 'kosambi',maxit,
-        sex.sp = F,verbose=F,parallel=T,cores)
+dropone.par <- function(cross,chr,prop=0.025,map.function = c("haldane",
+    "kosambi", "c-f", "morgan"),length.imp = 1, LOD.imp = 0,
+  maxit=1,sex.sp = F,verbose=F,parallel=T,error.prob = 0.03,cores=slurmcore)
 
-  Len <- quantile(as.numeric(cross.drops$Ldiff),p)
-  Lod <- quantile(as.numeric(cross.drops$LOD),p)
+  {
+  y <- round(sum(nmar(cross))*prop)
+  ### p = percent of longest markers to drop
+  print('starting parallel.droponemarker')
+  cross.drops <- parallel.droponemarker(cross,chr,maxit,cores,map.function='kosambi')
+
+  Len <- quantile(as.numeric(cross.drops$Ldiff),prop)
+  Lod <- quantile(as.numeric(cross.drops$LOD),prop)
 
   index.lod <- rownames(cross.drops[which(as.numeric(cross.drops$LOD) > Lod & as.numeric(cross.drops$LOD) > LOD.imp),])
   index.ldif <- rownames(cross.drops[which(as.numeric(cross.drops$Ldiff) > Len & as.numeric(cross.drops$Ldiff) > length.imp),])
@@ -149,7 +151,6 @@ all.crossed <- function(X){
   read.cross(format='csv',file=X,geno=c('AA','AB','BB'),
   alleles=c("A","B"))
 }
-
 reconst <- function(X,pop,dir){
   temp <- file.path(basedir,'rQTL',pop,paste('REMAPS/temp.',X,sep=''))
   myfiles <- lapply(temp, function(tocross){
@@ -187,21 +188,6 @@ reconst <- function(X,pop,dir){
   return(read.cross.jm(file=file.path(dir,'tempout'),format='csv',
     geno=c(1:3),estimate.map=FALSE))
 }
-
-#####
-
-# markersInInterval(cross, chr, min, max)
-
-# returns a list of marker names that fall within a range of locations
-
-# (in cM) along a linkage group (chr). Requires a single linkage group
-
-# output is a character vector
-
-#####
-
-
-
 markersInInterval <- function(cross, chr, min, max) {
 
  names(which(pull.map(cross=cross, chr=chr)[[chr]] < max &
@@ -209,52 +195,16 @@ markersInInterval <- function(cross, chr, min, max) {
              pull.map(cross=cross, chr=chr)[[chr]] > min))
 
 }
-
-
-
-#####
-
-# singleMarkerInInterval(cross, chr, min, max)
-
-# returns the marker name if only a single marker falls in a given
-
-# interval (including the possible case of NAs surrounding marker
-
-# on a linkage group, else returns value FALSE
-
-#####
-
-
-
 singleMarkerInInterval <- function(cross, chr, min, max) {
 
-  tmp <- markersInInterval(cross,chr,min,max)  
+  tmp <- markersInInterval(cross,chr,min,max)
 
   val <- ifelse(sum(!is.na(tmp) == 1 | is.na(tmp) == 2), tmp[!is.na(tmp)], FALSE)
 
 }
+removeDoubleXO <- function(cross, chr, verbose=TRUE) {
 
-#####
-
-# removeDoubleXO(cross,chr,verbose)
-
-# Takes a cross object, returns a cross object with all double-recombinant
-
-# genotypes removed.  This is useful to determine if the genotyping errors
-
-# from stacks influence the genetic maps.  if verbose = 1 (TRUE), minimal
-
-# reporting happens, if verbose > 1, all changed genotypes are written to
-
-# STDOUT
-
-#####
-
-
-
-removeDoubleXO <- function(cross, chr, verbose=TRUE) { 
-
-  if (!missing(chr)) 
+  if (!missing(chr))
 
       chr <- matchchr(chr, names(cross$geno))
 
@@ -274,7 +224,7 @@ removeDoubleXO <- function(cross, chr, verbose=TRUE) {
 
     # vector with estimated crossover locations
 
-    
+
 
     xo <- locateXO(cross, ch)
 
@@ -284,7 +234,7 @@ removeDoubleXO <- function(cross, chr, verbose=TRUE) {
 
     # of changes
 
-    
+
 
     total_removed <- 0
 
@@ -296,7 +246,7 @@ removeDoubleXO <- function(cross, chr, verbose=TRUE) {
 
     for (ind in 1:length(xo)) {            # loop through individuals
 
-      if (length(xo[[ind]]) <= 1) 
+      if (length(xo[[ind]]) <= 1)
 
         next  # skip individuals with one or fewer recombination events
 
@@ -310,7 +260,7 @@ removeDoubleXO <- function(cross, chr, verbose=TRUE) {
 
         # event and the next (location -1 thru location)
 
-        
+
 
         sMar <- singleMarkerInInterval(cross,ch,xo[[ind]][location-2],xo[[ind]][location+1])
 
@@ -347,9 +297,6 @@ removeDoubleXO <- function(cross, chr, verbose=TRUE) {
   cross
 
 }
-
-
-
 read.cross.jm <- function (format = c("csv", "csvr", "csvs", "csvsr", "mm", "qtx",
     "qtlcart", "gary", "karl", "mapqtl", "tidy"), dir = "", file,
     genfile, mapfile, phefile, chridfile, mnamesfile, pnamesfile,
@@ -507,10 +454,9 @@ read.cross.jm <- function (format = c("csv", "csvr", "csvs", "csvsr", "mm", "qtx
     #cat(" --Cross type:", class(cross)[1], "\n")
     cross
 }
-
-parallel.droponemarker <- function (cross, chr, error.prob = 0.03, map.function = c("haldane",
-    "kosambi", "c-f", "morgan"), m = 0, p = 0, maxit = 1, cores=slurmcore,
-    tol = 1e-06, sex.sp = FALSE, verbose = TRUE , parallel=T)
+parallel.droponemarker <- function (cross, chr, error.prob, map.function = c("haldane",
+    "kosambi", "c-f", "morgan"), m = 0, p = 0, maxit = 2, cores=slurmcore,
+    tol = 1e-06, sexsp = FALSE, verbose = TRUE , parallel=T)
 {
     if (!("cross" %in% class(cross)))
         stop("Input must have class \"cross\".")
@@ -526,12 +472,13 @@ parallel.droponemarker <- function (cross, chr, error.prob = 0.03, map.function 
         cross <- subset(cross, chr = tokeep)
     }
 
-    ##
+
     map.function <- match.arg(map.function)
     if (verbose)
         cat(" -Re-estimating map\n")
-    origmap <- est.map(cross, error.prob = error.prob, map.function = map.function,
-        m = m, p = p, maxit = maxit, tol = tol, sex.sp = sex.sp)
+    origmap <- qtl:::est.map(cross, error.prob = error.prob, map.function = map.function,
+        maxit = maxit, tol = tol, sex.sp = sex.sp,m = 0, p = 0)
+    cat(" Done Re-estimating map\n")
     cross <- replace.map(cross, origmap)
     origmaptab <- pull.map(cross, as.table = TRUE)
     origmaptab <- cbind(origmaptab, LOD = rep(NA, nrow(origmaptab)))
@@ -558,6 +505,7 @@ parallel.droponemarker <- function (cross, chr, error.prob = 0.03, map.function 
         temp <- subset(cross, chr = i)
 
         if (parallel) {
+              print('starting parallel droponemarker')
               registerDoParallel(cores)
               lod.dif <- foreach(j=seq(along=mnames),
                 .inorder=T,.combine='rbind',.packages = "qtl") %dopar% {
@@ -577,7 +525,7 @@ parallel.droponemarker <- function (cross, chr, error.prob = 0.03, map.function 
                 markerll <- qtl:::markerloglik(cross, mnames[j], error.prob)
 
                 newmap <- qtl:::est.map(drop.markers(temp, mnames[j]),
-                  error.prob = error.prob, map.function=map.function, m=m, p=p,
+                  error.prob = error.prob, map.function='kosambi', m=0, p=0,
                   maxit=maxit, tol=tol,sex.sp=FALSE)
 
 
@@ -593,7 +541,7 @@ parallel.droponemarker <- function (cross, chr, error.prob = 0.03, map.function 
               origmaptab[mnames,'LOD'] <- lod.dif[mnames,2]
               origmaptab[mnames,'Ldiff'] <- lod.dif[mnames,3]
 
-          } else { print('use qtl if multi cpus not avail')}
+          } else { print('use rqtl if multi cpus not avail')}
 
       }
       print('done with parallel on all chrs')
@@ -601,6 +549,5 @@ parallel.droponemarker <- function (cross, chr, error.prob = 0.03, map.function 
       origmaptab$chr <- factor(origmaptab$chr, levels = unique(origmaptab$chr))
       origmaptab
 }
-
 environment(read.cross.jm) <- asNamespace('qtl')
 environment(parallel.droponemarker) <- asNamespace('qtl')
