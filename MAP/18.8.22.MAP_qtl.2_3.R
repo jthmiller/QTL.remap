f9824b4cab8a1b1034e639b1bd3eede46ef7d5e9
Jeffrey Miller <jthmiller@ucdavis.edu>
2018-09-22 02:08:36 -0400

    com

--- MAP/18.8.22.MAP_qtl.2_3.R
+++ MAP/18.8.22.MAP_qtl.2_3.R
@@ -20,7 +20,7 @@ print('Dropping 2.5% of markers that inflate the map. Takes a long time...')
 
 ## Drop one marker, p is proportion of worst markers to drop
 system.time(
-  cross.18 <- dropone.par(cross=cross.18, prop=0.025,chr=X, maxit=1000, map.function = 'kosambi',
+  cross.18 <- dropone.par(cross=cross.18, prop=0.025,chr=X, maxit=2000, map.function = 'kosambi',
     length.imp = 1, LOD.imp = 0, error.prob=0.05, sex.sp = F, verbose=F, parallel=T, cores=slurmcore)
 )
 
@@ -36,14 +36,14 @@ cross.18 <- orderMarkers(cross.18,chr=X,window=5,use.ripple=T,
 
 print('Re-estimating the map')
 system.time(
-POS.map.18 <- est.map(cross.18,error.prob=0.05,map.function="kosambi", chr=X,maxit=1000)
+POS.map.18 <- est.map(cross.18,error.prob=0.05,map.function="kosambi", chr=X,maxit=2000)
 )
 cross.18 <- replace.map(cross.18, POS.map.18)
 
 ## Error rate
 print('determine error rate for last round of mapping')
 system.time(
-ers <- er.rate(cross=cross.18,cpus=slurmcore,maxit=1000)
+ers <- er.rate(cross=cross.18,cpus=slurmcore,maxit=2000)
 )
 
 print(paste(pop,'error rate for chromosome',X,'is',ers))
