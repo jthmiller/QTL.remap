87f1678ca6d3ef5a54ec7950b924e45ae9b68cb0
Jeffrey Miller <jthmiller@ucdavis.edu>
2018-09-23 02:58:06 -0400

    20 elr

--- MAP/18.8.22.MAP_qtl_1_ELR.sh
+++ MAP/18.8.22.MAP_qtl_1_ELR.sh
@@ -9,7 +9,7 @@
 #SBATCH -p high
 #####SBATCH --mail-type=ALL
 #####SBATCH --mail-user=jthmiller@ucdavis.edu
-#SBATCH --array=1-24
+#SBATCH --array=1-20
 
 ####QTLs are on chrm '1 2 8 13 18 24'
 scriptdir='/home/jmiller1/QTL_Map_Raw/popgen/rQTL/scripts/QTL_remap/MAP'
