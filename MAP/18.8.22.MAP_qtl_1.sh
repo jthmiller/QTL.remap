fb62a55bebaba432434bebd12d51d3f42d3108c1
Jeffrey Miller <jthmiller@ucdavis.edu>
2018-09-23 02:57:47 -0400

    20

--- MAP/18.8.22.MAP_qtl_1.sh
+++ MAP/18.8.22.MAP_qtl_1.sh
@@ -4,12 +4,12 @@
 #SBATCH -e /home/jmiller1/QTL_Map_Raw/popgen/rQTL/scripts/array_error_out/initial_map_err_%A_%a.txt
 #SBATCH --time=24:00:00
 #SBATCH --job-name=QTL.Remap
-#SBATCH --cpus-per-task=2
+#SBATCH --cpus-per-task=3
 #SBATCH --mem-per-cpu=5000
 #SBATCH -p high
 #####SBATCH --mail-type=ALL
 #####SBATCH --mail-user=jthmiller@ucdavis.edu
-#SBATCH --array=1-24
+#SBATCH --array=1-20
 
 ####QTLs are on chrm '1 2 8 13 18 24'
 scriptdir='/home/jmiller1/QTL_Map_Raw/popgen/rQTL/scripts/QTL_remap/MAP'
