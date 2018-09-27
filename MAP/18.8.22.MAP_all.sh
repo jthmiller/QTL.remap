215baa595150bd6e1685f8e98d7355ce0ac9bfd9
Jeffrey Miller <jthmiller@ucdavis.edu>
2018-09-26 22:59:18 -0400

    execuute

--- MAP/18.8.22.MAP_all.sh
+++ MAP/18.8.22.MAP_all.sh
@@ -6,7 +6,7 @@ scriptdir='/home/jmiller1/QTL_Map_Raw/popgen/rQTL/scripts/QTL_remap/MAP'
 
 echo "Population Chromosome Genotyping_Error_Rate" > /home/jmiller1/QTL_Map_Raw/popgen/rQTL/remap_out/genotyping_error_rate.txt
 
-RES=$(sbatch -J NBH.initial --parsable $scriptdir/18.8.22.MAP_qtl_1.sh NBH)
+RES=$(sbatch -J "NBH.initial" --parsable $scriptdir/18.8.22.MAP_qtl_1.sh NBH)
 RES2=$(sbatch -J ELR.initial --parsable --dependency=afterany:$RES $scriptdir/18.8.22.MAP_qtl_1.sh ELR)
 RES3=$(sbatch -J pardrop.initial --parsable --dependency=afterany:$RES2 $scriptdir/18.8.22.MAP_qtl_2.sh NBH)
 RES4=$(sbatch -J pardrop.initial --parsable --dependency=afterany:$RES3 $scriptdir/18.8.22.MAP_qtl_2.sh ELR)
--- MAP/18.8.22.MAP_qtl_1.sh
+++ MAP/18.8.22.MAP_qtl_1.sh
@@ -1,11 +1,9 @@
 #!/bin/bash -l
-#####SBATCH -J "initial_map"
 #SBATCH -o /home/jmiller1/QTL_Map_Raw/popgen/rQTL/scripts/array_error_out/out_%x_%A_%a.txt
 #SBATCH -e /home/jmiller1/QTL_Map_Raw/popgen/rQTL/scripts/array_error_out/err_%x_%A_%a.txt
 #SBATCH --time=24:00:00
-#SBATCH --job-name=QTL.Remap
 #SBATCH --cpus-per-task=2
-#SBATCH --mem-per-cpu=7000
+#SBATCH --mem-per-cpu=6000
 #SBATCH -p high
 #####SBATCH --mail-type=ALL
 #####SBATCH --mail-user=jthmiller@ucdavis.edu
@@ -17,6 +15,4 @@ scriptdir='/home/jmiller1/QTL_Map_Raw/popgen/rQTL/scripts/QTL_remap/MAP'
 
 pop=$1
 
-echo $1
-
-#Rscript $scriptdir/18.8.22.MAP_qtl.1_3.R --vanilla $pop $SLURM_ARRAY_TASK_ID $SLURM_CPUS_PER_TASK
+Rscript $scriptdir/18.8.22.MAP_qtl.1_3.R --vanilla $pop $SLURM_ARRAY_TASK_ID $SLURM_CPUS_PER_TASK
