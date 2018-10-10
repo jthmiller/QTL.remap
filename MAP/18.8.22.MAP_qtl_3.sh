9ade03686766b530bead1ab1e6c4793da712ba32
Jeffrey Miller <jthmiller@ucdavis.edu>
2018-10-10 17:06:13 -0400

    uncom time

--- MAP/18.8.22.MAP_qtl_3.sh
+++ MAP/18.8.22.MAP_qtl_3.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 #SBATCH -o /home/jmiller1/QTL_Map_Raw/popgen/rQTL/scripts/array_error_out/final/out_%x_%A_%a.txt
 #SBATCH -e /home/jmiller1/QTL_Map_Raw/popgen/rQTL/scripts/array_error_out/final/err_%x_%A_%a.txt
-#SBATCH -t 6:00:00
+#SBATCH -t 12:00:00
 #SBATCH --cpus-per-task=6
 #SBATCH --mem-per-cpu=2000
 #SBATCH -p med
