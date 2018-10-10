bcd1ed823f7e0c7fd589c288da11eff83fac064e
Jeffrey Miller <jthmiller@ucdavis.edu>
2018-10-10 17:04:07 -0400

    time 3

--- MAP/18.8.22.MAP_qtl_3.sh
+++ MAP/18.8.22.MAP_qtl_3.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -l
 #SBATCH -o /home/jmiller1/QTL_Map_Raw/popgen/rQTL/scripts/array_error_out/final/out_%x_%A_%a.txt
 #SBATCH -e /home/jmiller1/QTL_Map_Raw/popgen/rQTL/scripts/array_error_out/final/err_%x_%A_%a.txt
-#SBATCH --time=12:00:00
+#SBATCH -t 6:00:00
 #SBATCH --cpus-per-task=6
 #SBATCH --mem-per-cpu=2000
 #SBATCH -p med
-#SBATCH --array=1-24%24
-#SBATCH --mail-type=ALL
-#SBATCH --mail-user=jthmiller@ucdavis.edu
+#SBATCH --array=1-24%12
+####SBATCH --mail-type=ALL
+####SBATCH --mail-user=jthmiller@ucdavis.edu
 
 ####QTLs are on chrm '1 2 8 13 18 24'
 
