2d84f9138b4bcbc2ae8d94e43f0739c291845562
Jeffrey Miller <jthmiller@ucdavis.edu>
2018-10-15 00:56:41 -0400

    batchfile

--- MAP/control_file.R
+++ MAP/control_file.R
@@ -5,6 +5,9 @@ X <- as.numeric(Sys.getenv('SLURM_ARRAY_TASK_ID')) ## X is equal to chrom number
 slurmcore <- as.numeric(Sys.getenv('SLURM_CPUS_PER_TASK'))
 popq <- commandArgs(TRUE)[commandArgs(TRUE) %in% c('NBH','ELR','NEW','BP')]
 
+chrms <- c(1:24)
+pops <- c('NBH','ELR')
+
 ## Only use previously mapped markers?
 mapped.only=TRUE
 
