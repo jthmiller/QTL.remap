95106d7d2c7e7c5ba85bdd44cdff1eaab5c4d32f
Jeffrey Miller <jthmiller@ucdavis.edu>
2018-10-15 00:48:50 -0400

    path fix

--- QTL/18.8.22.MODEL.sh
+++ QTL/18.8.22.MODEL.sh
@@ -2,4 +2,4 @@
 
 scriptdir='/home/jmiller1/QTL_Map_Raw/popgen/rQTL/scripts/QTL_remap/QTL'
 
-RES1=$(sbatch -J "NBH.scan" --parsable $scriptdir/18.8.22.scan_qtl.sh NBH)
+RES1=$(sbatch -J "NBH.scan" --parsable $scriptdir/18.8.22.SCAN.sh NBH)
