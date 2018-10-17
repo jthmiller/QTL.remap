9f73fc0a963cd93bc0e010da6b6d175b83d34a1f
Jeffrey Miller <jthmiller@ucdavis.edu>
2018-10-17 15:32:17 -0400

    priority

--- QTL/18.8.22.SCAN.sh
+++ QTL/18.8.22.SCAN.sh
@@ -3,9 +3,9 @@
 #SBATCH -e /home/jmiller1/QTL_Map_Raw/popgen/rQTL/scripts/array_error_out/scan/err_%x_%A_%a.txt
 #SBATCH --time=48:00:00
 #SBATCH --job-name=QTL.Remap
-#SBATCH --cpus-per-task=12
+#SBATCH --cpus-per-task=24
 #SBATCH --mem-per-cpu=5000
-#SBATCH -p low
+#SBATCH -p high
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jthmiller@ucdavis.edu
 
