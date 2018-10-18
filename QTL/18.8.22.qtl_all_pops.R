1b36f89f97943731f223330b837b699e532bf84c
Jeffrey Miller <jthmiller@ucdavis.edu>
2018-10-18 12:29:28 -0400

    'force'

--- QTL/18.8.22.qtl_all_pops.R
+++ QTL/18.8.22.qtl_all_pops.R
@@ -10,3 +10,12 @@ NEW <- new.env()
 load('/home/jmiller1/QTL_Map_Raw/popgen/rQTL/NEW/REMAPS/QTLmap.Rsave', envir=NEW)
 ### These markers worked in NBH,NEW. Maybe ELR?
 marks <- unique(c(markernames(NBH$cross.18),markernames(NBH$cross.18)))
+
+cross.18 <- read.cross.jm(file=file.path(indpops,paste(pop,'.unphased.f2.csvr',sep='')),
+                format='csvr', geno=c(1:3),estimate.map=FALSE)
+
+cross.18 <- drop.markers(cross.18,markernames(cross.18)[!markernames(cross.18) %in% marks])
+
+gt <- geno.table(cross.18)
+cutoff <- 1.0e-4
+cross.18 <- drop.markers(cross.18,rownames(gt[gt$P.value<cutoff,]))
