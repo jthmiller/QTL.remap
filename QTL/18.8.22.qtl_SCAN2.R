2894530526bbba52e1820d635272b9550e55b4e1
Jeffrey Miller <jthmiller@ucdavis.edu>
2018-10-19 13:51:19 -0400

    sad

--- QTL/18.8.22.qtl_SCAN2.R
+++ QTL/18.8.22.qtl_SCAN2.R
@@ -13,12 +13,12 @@ registerDoParallel(slurmcore)
 perms2.norm.imp <- foreach(i=50,
   .combine=c,.packages = "qtl") %dopar% {
 
- operm <- scantwo(cross.18, chr, pheno.col=6, model="normal",
+ operm <- scantwo(cross.18, pheno.col=6, model="normal",
   method="imp",addcovar=NULL, intcovar=NULL, weights=NULL,
   use="complete.obs",incl.markers=FALSE, clean.output=FALSE,
   clean.nmar=1, clean.distance=0,maxit=4000, tol=1e-4,
   verbose=TRUE, n.perm=1 , perm.Xsp=FALSE, perm.strata=cross.18$pheno$stata,
-  assumeCondIndep=FALSE, batchsize=250, n.cluster=24)
+  assumeCondIndep=FALSE, batchsize=250, n.cluster=1)
 
 }
 
