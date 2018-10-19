9b27ab9f655ab17a1f4bd6c8187ccd8a92307612
Jeffrey Miller <jthmiller@ucdavis.edu>
2018-10-19 11:00:43 -0400

    scan2

--- QTL/18.8.22.qtl_SCAN2.R
+++ QTL/18.8.22.qtl_SCAN2.R
@@ -1,13 +1,32 @@
-try(
-cross.no2 <- subset(cross,chr=c(1,3:24))
+load('/home/jmiller1/QTL_Map_Raw/popgen/rQTL/NBH/REMAPS/QTLmap.Rsave')
+
+
+scan2.norm.imp <- scantwo(cross.18, pheno.col=6, model="normal",
+  method="imp",addcovar=NULL, intcovar=NULL, weights=NULL,
+  use="complete.obs",incl.markers=FALSE, clean.output=FALSE,
+  clean.nmar=1, clean.distance=0,maxit=4000, tol=1e-4,
+  verbose=TRUE, perm.Xsp=FALSE, perm.strata=NULL,
+  assumeCondIndep=FALSE, batchsize=250, n.cluster=24)
+
+
 registerDoParallel(slurmcore)
-operm.hk <- foreach(i=50,
+perms2.norm.imp <- foreach(i=50,
   .combine=c,.packages = "qtl") %dopar% {
-    operm <- scantwo(cross.no2, method="hk", n.perm=1,perm.strata=strata)
+
+ operm <- scantwo(cross.18, chr, pheno.col=6, model="normal",
+  method="imp",addcovar=NULL, intcovar=NULL, weights=NULL,
+  use="complete.obs",incl.markers=FALSE, clean.output=FALSE,
+  clean.nmar=1, clean.distance=0,maxit=4000, tol=1e-4,
+  verbose=TRUE, n.perm=1 , perm.Xsp=FALSE, perm.strata=cross.18$pheno$stata,
+  assumeCondIndep=FALSE, batchsize=250, n.cluster=24)
+
 }
+
 pen.em <- calc.penalties(operm.hk)
 save.image(paste('QTLmap.Rsave',sep=''))
-)
+
+
+
 try(
 ### qtl scan2
 registerDoParallel(slurmcore)
