0e7ea3574b311552f7b7b65d70c726a2255f379d
Jeffrey Miller <jthmiller@ucdavis.edu>
2018-10-22 00:34:21 -0400

    sh

--- MAP/18.8.22.MAP_all.sh
+++ MAP/18.8.22.MAP_all.sh
@@ -9,7 +9,7 @@ NBH2=$(sbatch -J "NBH.pardrop"  --parsable --dependency=afterany:$NBH1 $scriptdi
 NBH3=$(sbatch -J "NBH.final" --parsable --dependency=afterany:$NBH2 $scriptdir/MAP/18.8.22.MAP_qtl_3.sh NBH)
 NBH4=$(sbatch -J "NBH.scan" --parsable --dependency=afterany:$NBH3 $scriptdir/QTL/18.8.22.SCAN.sh NBH)
 NBH5=$(sbatch -J "NBH.scan2" --parsable --dependency=afterany:$NBH4 $scriptdir/QTL/18.8.22.SCAN2.sh NBH)
-
+NBH5=$(sbatch -J "NBH.scan2" --parsable --dependency=afterany:$NBH4 $scriptdir/QTL/compare.kinship2.sh NBH)
 #E. river
 ### RES4=$(sbatch -J "ELR.initial" --parsable $scriptdir/MAP/18.8.22.MAP_qtl_1.sh ELR)
 ELR1=$(sbatch -J "ELR.initial" --parsable --dependency=afterany:$NBH5 $scriptdir/MAP/18.8.22.MAP_qtl_1.sh ELR)
