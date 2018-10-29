ef3de1103d01c53cb1114dfafb86441f3206e003
Jeffrey Miller <jthmiller@ucdavis.edu>
2018-10-28 21:45:43 -0400

    pardrop

--- QTL/18.8.22.qtl_all_pops.R
+++ QTL/18.8.22.qtl_all_pops.R
@@ -1,28 +1,104 @@
 #!/bin/bash
 #source('/home/jmiller1/QTL_Map_Raw/popgen/rQTL/scripts/QTL_remap/MAP/control_file.R')
+source('/home/jmiller1/QTL_Map_Raw/popgen/rQTL/scripts/QTL_remap/MAP/debug.R')
 
 NBH <- new.env()
 load('/home/jmiller1/QTL_Map_Raw/popgen/rQTL/NBH/REMAPS/QTLmap.Rsave', envir=NBH)
 ELR <- new.env()
-#ELR <- load('/home/jmiller1/QTL_Map_Raw/popgen/rQTL/ELR/REMAPS/QTLmap.Rsave', envir=ELR)
+ELR <- load('/home/jmiller1/QTL_Map_Raw/popgen/rQTL/ELR/REMAPS/QTLmap.Rsave', envir=ELR)
 NEW <- new.env()
 load('/home/jmiller1/QTL_Map_Raw/popgen/rQTL/NEW/REMAPS/QTLmap.Rsave', envir=NEW)
 
-cbind(
-  NBH=summaryMap(NBH$cross.18)$length,
-  NEW=summaryMap(NBH$cross.18)$length,
-  ELR=summaryMap(NBH$cross.18)$length,
-
+cm.len <- cbind(NBH=summaryMap(NBH$cross.18)$length,
+  NEW=summaryMap(NEW$cross.18)$length
+#  ELR=summaryMap(ELR$cross.18)$length
 )
+cm.len.max <- apply(cm.len,1,max)
+cm.len <- cm.len.max-cm.len[,1:2]
+cm.len <- cm.len[1:24,]
+
+NBH$cross.18$ID <- paste('NBH',NBH$cross.18$ID,sep='_')
+NEW$cross.18$ID <- paste('NEW',NEW$cross.18$ID,sep='_')
+comp <- c(NBH$cross.18,NEW$cross.18)
+
+cross <- sim.geno(comp, n.draws=16, step=10, off.end=0,
+  error.prob=0.01,map.function="kosambi",stepwidth="fixed")
+
+cross.nbh <- subset(cross,ind=comp$pheno$cross1==1)
+cross.new <- subset(cross,ind=comp$pheno$cross2==1)
+
+scan.norm.imp.NBH <- scanone(cross.nbh, method="imp",model='normal',pheno.col=6)
+scan.norm.imp.NEW <- scanone(cross.new, method="imp",model='normal',pheno.col=6)
+
+#scan.norm.imp.NBH <- scanone(comp, method="imp",model='normal',pheno.col=6,ind.noqtl=comp$pheno$cross1==1)
+#scan.norm.imp.NEW <- scanone(comp, method="imp",model='normal',pheno.col=6,ind.noqtl=comp$pheno$cross2==1)
+#scan.norm.imp.cov <- scanone(comp, method="imp",model='normal',pheno.col=6,addcovar=comp$pheno$cross1)
+
+NBH.perm <- NBH$perms.norm.imp
+NEW.perm <- NEW$perms.norm.imp
+
+th.NBH <- summary(NBH.perm)[1,]
+th.NEW <- summary(NEW.perm)[1,]
+
+norm.qtl.NBH <- summary(scan.norm.imp.NBH, perms=NBH.perm, alpha=0.05)
+norm.qtl.NEW <- summary(scan.norm.imp.NEW, perms=NEW.perm, alpha=0.05)
+qtl.uns.NBH <- makeqtl(cross, chr=norm.qtl.NBH$chr, pos=norm.qtl.NBH$pos,what="draws")
+qtl.uns.NEW <- makeqtl(cross, chr=norm.qtl.NEW$chr, pos=norm.qtl.NEW$pos,what="draws")
+chrs.model <- unique(qtl.uns.NEW$chr, qtl.uns.NEW$chr)
+
+
+scan.all <- c(scan.norm.imp.NBH,scan.norm.imp.NEW)
+
+png('~/NBH_NEW.png')
+plot(scan.norm.imp.NBH, scan.norm.imp.NEW, chr=chrs.model, lodcolumn=1,
+  incl.markers=FALSE,lty=1, col=c("violetred","black"), lwd=2,
+  add=FALSE, gap=25, mtick = "line",
+  show.marker.names=FALSE, alternate.chrid=T,
+  bandcol=NULL, type="l", cex=1, pch=1, bg="grey",
+  bgrect=NULL)
+dev.off()
+
+
+
+for (X in 1:24){
+  ord <- order(as.numeric(gsub(paste(X,":",sep=''),'',markernames(NBH$cross.18,chr=X))))
+  NBH$cross.18 <- switch.order(NBH$cross.18, chr=X, ord, error.prob=0.01,
+    map.function="kosambi",maxit=100, tol=1e-6, sex.sp=F)
+  POS.map.18 <- est.map(NBH$cross.18,error.prob=0.01,map.function="kosambi", chr=X,maxit=10)
+  NBH$cross.18 <- replace.map(NBH$cross.18, POS.map.18)
+}
+
+for (X in 1:24){
+  ord <- order(as.numeric(gsub(paste(X,":",sep=''),'',markernames(NEW$cross.18,chr=X))))
+  NEW$cross.18 <- switch.order(NEW$cross.18, chr=X, ord, error.prob=0.01,
+    map.function="kosambi",maxit=100, tol=1e-6, sex.sp=F)
+  POS.map.18 <- est.map(NEW$cross.18,error.prob=0.01,map.function="kosambi", chr=X,maxit=10)
+  NEW$cross.18 <<- replace.map(NEW$cross.18, POS.map.18)
+}
+
+
+
+
+
+
+
+
+save.image(file.path('~/NEW.NBH.QTLmap.Rsave'))
+#perms.norm.imp <- scanone(comp, method="imp",model='normal',n.perm=5,perm.strata=cross.18$pheno$stata,pheno.col=6, n.cluster=slurmcore)
+
+
+
+
+
+
+scan.cat <- c(NBH$scan.norm.imp,NEW$scan.norm.imp)
+
 
 sapply(1:24,function(X){
     cmdist <-
 }
 )
 
-sim.geno(cross, n.draws=16, step=0, off.end=0, error.prob=0.0001,
-map.function=c("haldane","kosambi","c-f","morgan"),
-stepwidth=c("fixed", "variable", "max"))
 
 
 
