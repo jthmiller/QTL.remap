f1aed8e0061f7f99ae3f2aa19a4bc60d1b17b9ee
Jeffrey Miller <jthmiller@ucdavis.edu>
2018-10-30 16:15:47 -0400

    res

--- MAP/18.8.22.MAP_qtl_3.sh
+++ MAP/18.8.22.MAP_qtl_3.sh
@@ -4,7 +4,7 @@
 #SBATCH -t 12:00:00
 #SBATCH --cpus-per-task=24
 #SBATCH --mem-per-cpu=2000
-#SBATCH -p high
+#SBATCH -p med
 #SBATCH --array=1-24
 
 scriptdir='/home/jmiller1/QTL_Map_Raw/popgen/rQTL/scripts/QTL_remap/MAP'
