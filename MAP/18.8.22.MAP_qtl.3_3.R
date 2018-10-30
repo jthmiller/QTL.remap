3f34966e121419c02cb155856f0c5f365024afb9
Jeffrey Miller <jthmiller@ucdavis.edu>
2018-10-30 14:59:59 -0400

    4of4

--- MAP/18.8.22.MAP_qtl.3_3.R
+++ MAP/18.8.22.MAP_qtl.3_3.R
@@ -24,16 +24,16 @@ if (reorder.marks==T){
 print('Re-setimating map from filtered data on')
 cross.18 <- orderMarkers(cross.18,chr=X,window=5,use.ripple=T,
   error.prob=ers, map.function='kosambi',sex.sp=F,maxit=10000,tol=1e-3)
+} else {
+ripLod <- ripple(cross.18,chr=X, window=5, method="likelihood",
+  error.prob=0.01, map.function="kosambi",maxit=100,
+  tol=1e-6, sex.sp=FALSE, verbose=TRUE,n.cluster=slurmcore)
+new.ord <- ripLod[which.max(ripLod[,"LOD"]),]
+cross.18 <- switch.order(cross.18, X, new.ord)
+png(file.path(popdir,paste(X,'_order.png',sep='')))
+plot(gsub(paste(X,':',sep=''),markernames(cross.18)),main='position' )
+dev.off()
 }
-#else {
-#  print('Ripple at physical positions')
-#  cross.18 <- repRipple.jm(cross, chr = chrnames(cross)[1], window = 7,method = "countxo",
-#  verbose = T,map.function = "kosambi", sex.sp=F, clean1st = T, ripVerb = TRUE)
-    #ripVanWink <- ripple(cross.18, X, window=6, method="likelihood",
-    #   error.prob=ers, map.function="kosambi",maxit=100, tol=1e-6,
-    #   sex.sp=F, verbose=TRUE, n.cluster=slurmcore)
-    #cross.18 <- switch.order(cross.18, X, ripVanWink[1,])
-#}
 
 print('plotting LOD matrix')
 png(file.path(popdir,paste(X,'_RF_FINAL.png',sep='')))
@@ -57,36 +57,4 @@ cross.18$pheno$Pheno <- trsl.bin[as.character(cross.18$pheno$Pheno_05)]
 
 print('Re-writing the markers to rQTL format')
 write.cross(cross.18,filestem=paste(popdir,'/chr',X,'_',outname,'_3.QTLmap',sep=''),format="csv",chr=X)
-
-print('Adding un-genotyped individuals for stratified analysis')
-pheno.all <- phen <- read.table('/home/jmiller1/QTL_Map_Raw/popgen/rQTL/metadata/ALL_phenotype_Dist.txt',header=T)
-phen$Pheno_05 <- phen$pheno_all
-index <- which(phen$pop_all==pop)
-
-count.pheno <- sapply(0:5, function(pt){
-      pt <- as.character(pt)
-      total <- sum(phen$Pheno_05[index]==pt)
-      incross <- sum(cross.18$pheno$Pheno_05==pt)
-      return(as.numeric(total-incross))
-
-  }
-)
-names(count.pheno) <- as.character(0:5)
-count.pheno <- count.pheno[!is.na(count.pheno)]
-count.pheno <- rep.int(names(count.pheno), times=as.numeric(count.pheno))
-
-no_genos <- data.frame(Pheno=as.numeric(trsl.bin[as.character(count.pheno)]),sex=0,
-              ID=paste('NG',1:length(count.pheno),sep=''),Pheno_05=count.pheno,
-              markers= matrix('-',nrow=length(count.pheno),ncol=as.numeric(nmar(cross.18))))
-
-phenos <- data.frame(count.pheno,Pheno=as.numeric(trsl.bin[as.character(count.pheno)]))
-rownames(phenos) <- paste('NG',1:length(count.pheno),sep='')
-
-write.table(no_genos,file=file.path(popdir,'no_genos.csv'),
-  col.names=F,row.names=F,quote=F,sep=',')
-
-system(paste('cat ',popdir,'/chr',X,'_',outname,'_3.QTLmap.csv ',popdir,'/no_genos.csv > ',popdir,'/temp.',X,sep=''))
-
-print('saving... done with mapping ind chromosomes')
-
 save.image(paste(popdir,'/chr',X,'_',outname,'.QTLmap.Rsave',sep=''))
---
+++ MAP/18.8.22.MAP_qtl.4_4.R
@@ -0,0 +1,36 @@
+##Rscript
+
+paste(popdir,'/chr',X,'_',outname,'_3.QTLmap',sep='')
+
+print('Adding un-genotyped individuals for stratified analysis')
+pheno.all <- phen <- read.table('/home/jmiller1/QTL_Map_Raw/popgen/rQTL/metadata/ALL_phenotype_Dist.txt',header=T)
+phen$Pheno_05 <- phen$pheno_all
+index <- which(phen$pop_all==pop)
+
+count.pheno <- sapply(0:5, function(pt){
+      pt <- as.character(pt)
+      total <- sum(phen$Pheno_05[index]==pt)
+      incross <- sum(cross.18$pheno$Pheno_05==pt)
+      return(as.numeric(total-incross))
+
+  }
+)
+names(count.pheno) <- as.character(0:5)
+count.pheno <- count.pheno[!is.na(count.pheno)]
+count.pheno <- rep.int(names(count.pheno), times=as.numeric(count.pheno))
+
+no_genos <- data.frame(Pheno=as.numeric(trsl.bin[as.character(count.pheno)]),sex=0,
+              ID=paste('NG',1:length(count.pheno),sep=''),Pheno_05=count.pheno,
+              markers= matrix('-',nrow=length(count.pheno),ncol=as.numeric(nmar(cross.18))))
+
+phenos <- data.frame(count.pheno,Pheno=as.numeric(trsl.bin[as.character(count.pheno)]))
+rownames(phenos) <- paste('NG',1:length(count.pheno),sep='')
+
+write.table(no_genos,file=file.path(popdir,'no_genos.csv'),
+  col.names=F,row.names=F,quote=F,sep=',')
+
+system(paste('cat ',popdir,'/chr',X,'_',outname,'_3.QTLmap.csv ',popdir,'/no_genos.csv > ',popdir,'/temp.',X,sep=''))
+
+print('saving... done with mapping ind chromosomes')
+
+save.image(paste(popdir,'/chr',X,'_',outname,'.QTLmap.Rsave',sep=''))
