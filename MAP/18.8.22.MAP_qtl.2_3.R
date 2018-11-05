50a88466cdd9cd47e2232ea553071617d40dab1e
Jeffrey Miller <jeffreymiller@campus-058-012.ucdavis.edu>
2018-11-05 14:37:50 -0800

    rerun pardrop

--- MAP/18.8.22.MAP_qtl.2_3.R
+++ MAP/18.8.22.MAP_qtl.2_3.R
@@ -12,13 +12,17 @@ cross.18 <- replacemap(cross.18, zero.map)
 print(summary(pull.map(cross.18))[as.character(X), ])
 
 marker.warning()
-# print('Dropping ~6 markers that inflate the map. Takes a long time...') Drop
-# one marker, p is proportion of worst markers to drop cross.18 <-
-# dropone.par(cross.18, X, drop.its = 3, maxit = 5, map.function = 'kosambi',
-# error.prob = 0.03, sex.sp = F, verbose = F, parallel = T, cores = slurmcore)
+print("Dropping ~3 markers that inflate the map. Takes a long time...")
 
+# cross.18 <- dropone.par(cross.18, X, drop.its = 3, maxit = 5, map.function =
+# 'kosambi', error.prob = 0.03, sex.sp = F, verbose = F, parallel = T, cores =
+# slurmcore)
 
-cross.18 <- dropByDropone(cross = cross.18, droponeRes = dropone, endMarkerThresh = 10, 
+dropone <- parallel.droponemarker(cross, chr, error.prob = 0.03, map.function = "kosambi", 
+  m = 0, p = 0, maxit = 5, cores = slurmcore, tol = 1e-06, sex.sp = FALSE, verbose = F, 
+  parallel = T)
+
+cross1 <- dropByDropone(cross = cross.18, droponeRes = dropone, endMarkerThresh = 15, 
   re.est.map = F)
 
 marker.warning()
@@ -31,7 +35,8 @@ if (reorder.marks == T) {
 
 print("Re-estimating the map")
 POS.map.18 <- est.map(cross.18, error.prob = 0.05, map.function = "kosambi", chr = X, 
-  maxit = 3000)
+  maxit = 1000)
+
 cross.18 <- replace.map(cross.18, POS.map.18)
 
 print(summary(pull.map(cross.18))[as.character(X), ])
