8de0866b323ebef68356e15248e2aa2cba348085
Jeffrey Miller <jeffreymiller@campus-058-012.ucdavis.edu>
2018-11-05 23:27:54 -0800

    med

--- MAP/18.8.22.MAP_all.sh
+++ MAP/18.8.22.MAP_all.sh
@@ -10,12 +10,13 @@ sbatch -J "kinship" $scriptdir/MAP/compare.kinship.sh
 #New Bedford Harbor
 NBH1=$(sbatch -J "NBH.initial" --parsable $scriptdir/MAP/18.8.22.MAP_qtl_1.sh NBH)
 NBH2=$(sbatch -J "NBH.pardrop"  --parsable --dependency=afterany:$NBH1 $scriptdir/MAP/18.8.22.MAP_qtl_2.sh NBH)
-NBH2=$(sbatch -J "NBH.pardrop"  --parsable $scriptdir/MAP/18.8.22.MAP_qtl_2.sh NBH)
-NBH2=$(sbatch -J "NBH.pardrop"  --parsable --dependency=afterany:$NBH1 $scriptdir/MAP/18.8.22.MAP_qtl_2.sh NBH)
-NBH3=$(sbatch -J "NBH.ripple" --parsable $scriptdir/MAP/18.8.22.MAP_qtl_3.sh NBH)
+#NBH2=$(sbatch -J "NBH.pardrop"  --parsable $scriptdir/MAP/18.8.22.MAP_qtl_2.sh NBH)
 NBH3=$(sbatch -J "NBH.ripple" --parsable --dependency=afterany:$NBH2 $scriptdir/MAP/18.8.22.MAP_qtl_3.sh NBH)
+#NBH3=$(sbatch -J "NBH.ripple" --parsable $scriptdir/MAP/18.8.22.MAP_qtl_3.sh NBH)
 NBH4=$(sbatch -J "NBH.final" --parsable --dependency=afterany:$NBH3 $scriptdir/MAP/18.8.22.MAP_qtl_4.sh NBH)
+#NBH4=$(sbatch -J "NBH.final" --parsable $scriptdir/MAP/18.8.22.MAP_qtl_4.sh NBH)
 NBH5=$(sbatch -J "NBH.scan" --parsable --dependency=afterany:$NBH4 $scriptdir/QTL/18.8.22.SCAN.sh NBH)
+#NBH5=$(sbatch -J "NBH.scan" --parsable $scriptdir/QTL/18.8.22.SCAN.sh NBH)
 NBH6=$(sbatch -J "NBH.scan2" --parsable --dependency=afterany:$NBH5 $scriptdir/QTL/18.8.22.SCAN2.sh NBH)
 #NBH5=$(sbatch -J "NBH.parents" --parsable --dependency=afterany:$NBH4 $scriptdir/QTL/compare.kinship2.sh NBH)
 #E. river
@@ -28,7 +29,11 @@ ELR6=$(sbatch -J "ELR.scan2" --parsable --dependency=afterany:$ELR5 $scriptdir/Q
 #Newark
 NEW1=$(sbatch -J "NEW.initial" --parsable $scriptdir/MAP/18.8.22.MAP_qtl_1.sh NEW)
 NEW2=$(sbatch -J "NEW.pardrop"  --parsable --dependency=afterany:$NEW1 $scriptdir/MAP/18.8.22.MAP_qtl_2.sh NEW)
+#NEW2=$(sbatch -J "NEW.pardrop"  --parsable $scriptdir/MAP/18.8.22.MAP_qtl_2.sh NEW)
 NEW3=$(sbatch -J "NEW.ripple" --parsable --dependency=afterany:$NEW2 $scriptdir/MAP/18.8.22.MAP_qtl_3.sh NEW)
+#NEW3=$(sbatch -J "NEW.ripple" --parsable $scriptdir/MAP/18.8.22.MAP_qtl_3.sh NEW)
 NEW4=$(sbatch -J "NEW.final" --parsable --dependency=afterany:$NEW3 $scriptdir/MAP/18.8.22.MAP_qtl_4.sh NEW)
+#NEW4=$(sbatch -J "NEW.final" --parsable $scriptdir/MAP/18.8.22.MAP_qtl_4.sh NEW)
 NEW5=$(sbatch -J "NEW.scan" --parsable --dependency=afterany:$NEW4 $scriptdir/QTL/18.8.22.SCAN.sh NEW)
+#NEW5=$(sbatch -J "NEW.scan" --parsable $scriptdir/QTL/18.8.22.SCAN.sh NEW)
 NEW6=$(sbatch -J "NEW.scan2" --parsable --dependency=afterany:$NEW5 $scriptdir/QTL/18.8.22.SCAN2.sh NEW)
