5fb99e81a6ce5864b611d55d724e15abb5955974
Jeffrey Miller <jeffreymiller@Jeffreys-MacBook-Pro-2.local>
2018-11-23 05:59:31 -0500

    elr

--- qtlBim.manymarks.R
+++ qtlBim.manymarks.R
@@ -2,12 +2,12 @@
 debug.cross <- T
 source("/home/jmiller1/QTL_Map_Raw/popgen/rQTL/scripts/QTL_remap/MAP/control_file.R")
 library("qtlbim")
-
-# cross.18 <- reconst(X = chrms, pop = popq, temp.dir = popdir, a = 2)
+clean <- qtl::clean
+cross.18 <- reconst(X = chrms, pop = popq, temp.dir = popdir, a = 2)
 
 # print('Writing the merged chromosome markers to rQTL format')
-# write.cross(cross.18, filestem = paste(popdir, '/', outname,
-# '.BACKUP.QTL_chr.QTLmap', sep = ''), format = 'csv')
+write.cross(cross.18, filestem = paste(popdir, "/", outname, ".BACKUP.QTL_chr.QTLmap", 
+  sep = ""), format = "csv")
 
 popdir <- "/home/jmiller1/QTL_Map_Raw/popgen/rQTL/NEW/REMAPS"
 popdir <- "/home/jmiller1/QTL_Map_Raw/popgen/rQTL/NBH/REMAPS"
@@ -20,22 +20,31 @@ cross.18 <- read.cross(format = "csv", dir = popdir, file = paste(outname, ".BAC
 crOb <- cross.18
 crOb <- qb.genoprob(crOb, step = 1)
 qbData.nbh <- qb.data(crOb, pheno.col = 1, trait = "ordinal")
-qbModel.nbh <- qb.model(crOb, epistasis = T, main.nqtl = 2, interval = 30, chr.nqtl = rep(2, 
+qbModel.nbh <- qb.model(crOb, epistasis = T, main.nqtl = 2, interval = 1200, chr.nqtl = rep(1, 
   nchr(crOb)), mean.nqtl = 2, depen = FALSE)
-mc <- qb.mcmc(crOb, qbData.nbh, qbModel.nbh, pheno.col = 1, n.iter = 2000)
+qbModel.nbh <- qb.model(crOb, epistasis = T, main.nqtl = 4, mean.nqtl = 5, depen = FALSE)
+
+mc <- qb.mcmc(crOb, qbData.nbh, qbModel.nbh, pheno.col = 1, n.iter = 3000)
 so <- qb.scanone(mc, epistasis = T, type = "2logBF")
 so.LPD <- qb.scanone(mc, epistasis = T, type = "LPD")
 st <- qb.scantwo(mc, chr = c(1, 2, 8, 18, 24))
 st.LPD <- qb.scantwo(mc, chr = c(1, 2, 8, 18, 24), type = "LPD")
 
-png("/home/jmiller1/public_html/scanone.nbh.so.png", width = 2000)
+png("/home/jmiller1/public_html/scan_diagnost.so.png", width = 2000)
+plot(qb.coda(mc, variables = c("nqtl")))
+plot(qb.hpdone(mc))
+plot(qb.epistasis(mc))
+dev.off()
+
+
+png("/home/jmiller1/public_html/scanone.so.png", width = 2000)
 plot(so, chr = c(1:24))
 dev.off()
 
-png("/home/jmiller1/public_html/scanone.nbh.so.LPD.png", width = 2000)
+png("/home/jmiller1/public_html/scanone.so.LPD.png", width = 2000)
 plot(so.LPD, c(1:24))
 dev.off()
 
-png("/home/jmiller1/public_html/scanone.nbh.st.png", width = 2000, height = 2000)
+png("/home/jmiller1/public_html/scanone.st.png", width = 2000, height = 2000)
 plot(st, c(1:24))
 dev.off()
