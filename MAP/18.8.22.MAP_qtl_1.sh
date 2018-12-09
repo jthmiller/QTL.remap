0bece94a4f090bd0d68dfdd7627e750a38317a95
Jeffrey Miller <jeffreymiller@Jeffreys-MacBook-Pro-2.local>
2018-12-09 10:21:38 -0500

    elr

--- MAP/18.8.22.MAP_qtl_1.sh
+++ MAP/18.8.22.MAP_qtl_1.sh
@@ -3,17 +3,19 @@
 #SBATCH -e /home/jmiller1/QTL_Map_Raw/popgen/rQTL/scripts/array_error_out/inital/err_%x_%A_%a.txt
 #SBATCH --time=24:00:00
 #SBATCH --cpus-per-task=2
-#SBATCH --mem-per-cpu=2500
-#SBATCH -p high
+#SBATCH --mem-per-cpu=3000
+#SBATCH -p med
 #SBATCH --array=1-24
 
 scriptdir='/home/jmiller1/QTL_Map_Raw/popgen/rQTL/scripts/QTL_remap/MAP'
 
 pop=$1
 
+if [ $pop = "BRP" ]; then
+  Rscript $scriptdir/manymarks2_bp.R --vanilla $pop $SLURM_ARRAY_TASK_ID $SLURM_CPUS_PER_TASK
 if [ $pop = "ELR" ]; then
-	Rscript $scriptdir/manymarks2.R --vanilla $pop $SLURM_ARRAY_TASK_ID $SLURM_CPUS_PER_TASK
+    Rscript $scriptdir/manymarks2_elr.R --vanilla $pop $SLURM_ARRAY_TASK_ID $SLURM_CPUS_PER_TASK
 else
-	Rscript $scriptdir/manymarks2_elr.R --vanilla $pop $SLURM_ARRAY_TASK_ID $SLURM_CPUS_PER_TASK
+  Rscript $scriptdir/manymarks2.R --vanilla $pop $SLURM_ARRAY_TASK_ID $SLURM_CPUS_PER_TASK
 fi
 ####Rscript $scriptdir/manymarks2.R --vanilla $pop $SLURM_ARRAY_TASK_ID $SLURM_CPUS_PER_TASK
