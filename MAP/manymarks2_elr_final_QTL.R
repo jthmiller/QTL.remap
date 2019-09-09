09b4679123ff5e10f080c530ddac7a33fffd3b3d
jtmiller <jtmiller@umn.edu>
2019-09-09 10:31:49 -0500

    sdf

--- MAP/manymarks2_elr_final_QTL.R
+++ MAP/manymarks2_elr_final_QTL.R
@@ -8,9 +8,9 @@ library('qtl')
 ## read in the QTL cross
 ################################################################################
 mpath <- '/home/jmiller1/QTL_Map_Raw/ELR_final_map'
-i <- 18
-cross.r <- read.cross(
- file = file.path(mpath, paste0('ELR_chr_',i,'.csv')),
+
+cross <- read.cross(
+ file = file.path(mpath, 'ELR.all.csv'),
  format = "csv", genotypes=c("AA","AB","BB"), alleles=c("A","B"),
  estimate.map = FALSE
 )
@@ -23,8 +23,25 @@ cross.final.nodups <- drop.markers(cross.r, unlist(dups))
 
 cross_nodups <- orderMarkers(cross.final.nodups, window=7,verbose=FALSE,
                  use.ripple=TRUE, error.prob=0.01, sex.sp=FALSE,
-                 map.function="kosambi",maxit=2000, tol=1e-4)
+                 map.function="kosambi",maxit=4000, tol=1e-4)
 
 filename <- paste0('/home/jmiller1/QTL_Map_Raw/ELR_final_map/ELR_mapped_chr_',i)
 write.cross(cross_nodups,chr=i,filestem=filename,format="csv")
 ################################################################################
+
+
+################################################################################
+for i in ELR*.csv; do
+ cut -d',' -f6- $i > ${i%.csv}.nofirst
+done
+
+paste *.nofirst > ELR_marks
+cut -d',' -f1-5 ELR_chr_24.csv > ELR.info
+
+paste -d',' ELR.info ELR_marks > ELR.all.scv
+awk -F',' '{print $0}' ELR.all.scv > ELR.all.csv
+################################################################################
+
+
+perms.bin.em <- scanone(cross.r, method = "em", model = "binary", maxit = 1000,
+  n.perm = 500, pheno.col = 4, n.cluster = 1)
